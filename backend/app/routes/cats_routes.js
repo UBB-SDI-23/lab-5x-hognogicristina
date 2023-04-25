51922b46e33d719af3fd15f2bb8cf1c12188a3ae
Hognogi Cristina <hognogicristina@gmail.com>
2023-04-25 09:02:15 +0300

    Lab5

--- backend/app/routes/cats_routes.js
+++ backend/app/routes/cats_routes.js
@@ -1,11 +1,25 @@
 module.exports = (app) => {
     const controllerCat = require("../controllers/cats_controller.js")
 
-    app.get('/cats', controllerCat.getCat)
-    app.get('/cats/:id', controllerCat.getOneCat)
-    app.post('/cats_add', controllerCat.createCat)
-    app.delete('/cats_delete/:id', controllerCat.deleteCat)
-    app.put('/cats_update/:id', controllerCat.updateCat)
-    app.get('/cats_filter/:weight', controllerCat.filterCat)
-    app.get('/cats_statistic/:breed', controllerCat.getStatisticsBreed)
+    app.get('/cats', controllerCat.getCat, (req, res) => {
+        res.header("Access-Control-Allow-Origin", "https://meow-adopt-a-cat.netlify.app")
+    })
+    app.get('/cats/:id', controllerCat.getOneCat, (req, res) => {
+        res.header("Access-Control-Allow-Origin", "https://meow-adopt-a-cat.netlify.app")
+    })
+    app.post('/cats_add', controllerCat.createCat, (req, res) => {
+        res.header("Access-Control-Allow-Origin", "https://meow-adopt-a-cat.netlify.app")
+    })
+    app.delete('/cats_delete/:id', controllerCat.deleteCat, (req, res) => {
+        res.header("Access-Control-Allow-Origin", "https://meow-adopt-a-cat.netlify.app")
+    })
+    app.put('/cats_update/:id', controllerCat.updateCat, (req, res) => {
+        res.header("Access-Control-Allow-Origin", "https://meow-adopt-a-cat.netlify.app")
+    })
+    app.get('/cats_filter/:weight', controllerCat.filterCat, (req, res) => {
+        res.header("Access-Control-Allow-Origin", "https://meow-adopt-a-cat.netlify.app")
+    })
+    app.get('/cats_statistic/:breed', controllerCat.getStatisticsBreed, (req, res) => {
+        res.header("Access-Control-Allow-Origin", "https://meow-adopt-a-cat.netlify.app")
+    })
 }
--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "https://644767ba21d4b003fd837225--meow-adopt-a-cat.netlify.app")
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 	next()
 })
 
