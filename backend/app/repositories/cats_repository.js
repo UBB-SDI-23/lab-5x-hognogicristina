5f727662d2546805a93b8a31d4fe282bdd8f5af8
Hognogi Cristina <hognogicristina@gmail.com>
2023-04-30 20:34:03 +0300

    Lab7

--- backend/app/repositories/cats_repository.js
+++ backend/app/repositories/cats_repository.js
@@ -7,21 +7,19 @@ module.exports = {
     genCat: function () {
         cats = []
 
-        let cat1 = new cat({ name: "Mittens", age: 3, color: 'white', breed: 'Persian', weight: 10, ownerId: 1 })
-        let cat2 = new cat({ name: "Fluffy", age: 4, color: 'black', breed: 'Bombay', weight: 8, ownerId: 10 })
-        let cat3 = new cat({ name: "Patches", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 3 })
-        let cat4 = new cat({ name: "Zoe", age: 1, color: 'white', breed: 'Khao Manee', weight: 3, ownerId: 10 })
-        let cat5 = new cat({ name: "Misty", age: 5, color: 'black', breed: 'Siamese', weight: 9, ownerId: 2 })
-        let cat6 = new cat({ name: "Smokey", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 9 })
-        let cat7 = new cat({ name: "Tiger", age: 3, color: 'white', breed: 'Persian', weight: 10, ownerId: 1 })
-        let cat8 = new cat({ name: "Shadow", age: 4, color: 'black', breed: 'Bombay', weight: 8, ownerId: 4 })
-        let cat9 = new cat({ name: "Oreo", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 4 })
-        let cat10 = new cat({ name: "Socks", age: 1, color: 'white', breed: 'Khao Manee', weight: 3, ownerId: 8 })
-        let cat11 = new cat({ name: "Snowball", age: 5, color: 'black', breed: 'Siamese', weight: 9, ownerId: 5 })
-        let cat12 = new cat({ name: "Salem", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 6 })
-        let cat13 = new cat({ name: "Sylvester", age: 3, color: 'white', breed: 'Persian', weight: 10, ownerId: 7 })
-        let cat14 = new cat({ name: "Garfield", age: 4, color: 'black', breed: 'Bombay', weight: 8, ownerId: 9 })
-        let cat15 = new cat({ name: "Tom", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 9 })
+        let cat1 = new cat({ name: "Mittens", age: 3, color: 'white', breed: 'Persian', weight: 10, ownerId: 1, description: 'Mittens is a cute and cuddly cat with a playful personality.' })
+        let cat2 = new cat({ name: "Fluffy", age: 4, color: 'black', breed: 'Bombay', weight: 8, ownerId: 10, description: 'Fluffy is a mischievous cat with a love for adventure.' })
+        let cat3 = new cat({ name: "Patches", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 3, description: 'Patches is a friendly cat who loves attention and cuddles.' })
+        let cat4 = new cat({ name: "Zoe", age: 1, color: 'white', breed: 'Khao Manee', weight: 3, ownerId: 10, description: 'Zoe is a curious cat who loves exploring her surroundings.' })
+        let cat5 = new cat({ name: "Misty", age: 5, color: 'black', breed: 'Siamese', weight: 9, ownerId: 2, description: 'Misty is a graceful cat with a quiet and calm demeanor.' })
+        let cat6 = new cat({ name: "Smokey", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 9, description: 'Smokey is a playful cat who loves chasing toys and cuddling.' })
+        let cat7 = new cat({ name: "Tiger", age: 3, color: 'white', breed: 'Persian', weight: 10, ownerId: 1, description: 'Tiger is a majestic cat with a regal presence and a friendly demeanor.' })
+        let cat8 = new cat({ name: "Shadow", age: 4, color: 'black', breed: 'Bombay', weight: 8, ownerId: 4, description: 'Shadow is a mysterious cat with a quiet and watchful nature.' })
+        let cat9 = new cat({ name: "Oreo", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 4, description: 'Oreo is a sweet cat who loves snuggles and treats.' })
+        let cat10 = new cat({ name: "Socks", age: 1, color: 'white', breed: 'Khao Manee', weight: 3, ownerId: 8, description: 'Socks is a playful and energetic cat who loves to play and explore.' })
+        let cat11 = new cat({ name: "Snowball", age: 5, color: 'black', breed: 'Siamese', weight: 9, ownerId: 5, description: 'Snowball is a curious and adventurous cat who loves to explore and discover new things.' })
+        let cat12 = new cat({ name: "Salem", age: 2, color: 'brown', breed: 'Tabby', weight: 6, ownerId: 6, description: 'Salem is a mischievous and playful cat who loves to get into trouble and have fun.' })
+        let cat13 = new cat({ name: "Sylvester", age: 3, color: 'white', breed: 'Persian', weight: 10, ownerId: 7, description: 'Sylvester is a lovable and cuddly cat who loves to cuddle and be petted.' })
 
         cats.push(cat1)
         cats.push(cat2)
@@ -69,15 +67,15 @@ module.exports = {
         let index = cats.filter(el => el.id == id)
 
         cats[index] = {
-			id: parseInt(id),
-			name: name,
+            id: parseInt(id),
+            name: name,
             age: age,
             color: color,
             breed: breed,
             weight: weight,
             description: description,
             ownerId: ownerId
-		}
+        }
 
         return await db.updateCat(cats[index])
     },
@@ -86,7 +84,7 @@ module.exports = {
         return await db.filterCatsByWeight(weight)
     },
 
-    getStatisticReport: async function() {
+    getStatisticReport: async function () {
         return await db.getStatisticReport()
     },
 
